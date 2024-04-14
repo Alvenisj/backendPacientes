93e49a0e8409df1019b072e0ac0e3d64e2a48ecc
Alvenis Becerra <alvenisj@gmail.com>
2024-04-14 00:16:24 -0500

    Backend completo con nodeJs

--- routes/pacienteRoutes.js
+++ routes/pacienteRoutes.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import checkAutenticacion from '../middleware/autenticacionMiddleware.js';
+import express from "express";
+import checkAutenticacion from "../middleware/autenticacionMiddleware.js";
 
-import { registrarPacientes,
-         obtenerPacientes,
-         obtenerUnSoloPaciente,
-         actualizarPaciente,
-         eliminarPaciente } from '../controllers/pacienteController.js'
+import {
+  registrarPacientes,
+  obtenerPacientes,
+  obtenerUnSoloPaciente,
+  actualizarPaciente,
+  eliminarPaciente,
+} from "../controllers/pacienteController.js";
 
 // INSTANCIAMOS EL ROUTER DE LOS PACIENTES
 const router = express.Router();
 
-
 // UNA FORMA DE HACER DOS MÉTODOS HACIA LA MISMA RUTA RAÍZ DE MANERA ENCADENADA
-router.route('/')
-    .post( checkAutenticacion, registrarPacientes )
-    .get( checkAutenticacion, obtenerPacientes );
+router
+  .route("/")
+  .post(checkAutenticacion, registrarPacientes)
+  .get(checkAutenticacion, obtenerPacientes);
 
-router.route('/:id')
-    .get( checkAutenticacion, obtenerUnSoloPaciente )
-    .put( checkAutenticacion, actualizarPaciente )
-    .delete( checkAutenticacion, eliminarPaciente );
-
-
-
+router
+  .route("/:id")
+  .get(checkAutenticacion, obtenerUnSoloPaciente)
+  .put(checkAutenticacion, actualizarPaciente)
+  .delete(checkAutenticacion, eliminarPaciente);
 
 // EXPORTAMOS EL ARCHIVO DE LA RUTAS DE LOS PACIENTES
 export default router;
